17e4521a501c987e8baf495582bd97606a7b9ae6
craigdomingo9 <craigdomingo9@gmail.com>
2025-07-26 06:19:08 +0200

    minor fix

--- frontend/src/app/listings/page.tsx
+++ frontend/src/app/listings/page.tsx
@@ -7,7 +7,7 @@ type Props = {
 async function page({searchParams}: Props) {
 
   // Copy searchParams to avoid direct mutation
-  const params = { ...searchParams };
+  const params = { ...(await searchParams) };
 
   // Extract amenities (could be string or string[])
   const amenities = params.amenity;
