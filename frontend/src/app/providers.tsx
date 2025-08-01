e02b41f1f5a8df354430edb9e13bb0ef161e54fa
craigdomingo9 <craigdomingo9@gmail.com>
2025-08-01 03:21:14 +0200

    enabled tanstack query extension

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,15 +2,31 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+
+const MyQueryClient = new QueryClient({/* ... */});
+
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__:
+      import("@tanstack/query-core").QueryClient;
+  }
+}
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [client] = useState(() => new QueryClient());
+  
+  useEffect(() => {
+    // This code is for all users
+    window.__TANSTACK_QUERY_CLIENT__ = MyQueryClient;
+  }, [])
 
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={MyQueryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
