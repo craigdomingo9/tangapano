e08b9390a7234544984d2e04c7031aad9343f1cf
craigdomingo9 <craigdomingo9@gmail.com>
2025-08-15 02:07:09 +0200

    hot reload -> docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,14 @@ module.exports = {
       },
     ],
   },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 0,
+      ignored: ["**/node_modules"],
+    };
+    return config;
+  },
   devIndicators: false,
   allowedDevOrigins: ["192.168.43.242"],
 };
