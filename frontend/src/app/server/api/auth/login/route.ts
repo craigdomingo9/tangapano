13b537636ccad71881a4bf3165cd1341ed515327
root <root@server.tangapano.co.zw>
2025-09-06 09:53:28 +0000

    .

--- frontend/src/app/server/api/auth/login/route.ts
+++ frontend/src/app/server/api/auth/login/route.ts
@@ -30,7 +30,8 @@ export async function POST(req: NextRequest) {
     response.cookies.set("auth_token", token, {
       httpOnly: true,
       path: "/",
-      secure: process.env.NODE_ENV === "production",
+      // TODO: fix secure attribute
+      secure: false, //process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 60 * 60 * 24 * 30, // 30 days
       expires: 1000 * 60 * 60 * 24 * 30, // 30 days
