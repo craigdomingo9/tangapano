f54741bf03d2373c3db53de2d300b7021115ce99
craigdomingo9 <craigdomingo9@gmail.com>
2025-09-20 10:18:50 +0200

    amenity icons on listings page

--- frontend/src/components/Listings/ListingAmenities.tsx
+++ frontend/src/components/Listings/ListingAmenities.tsx
@@ -1,5 +1,6 @@
 import fetchAmenities from "@/lib/services/api/fetchAmenities";
 import { useQuery } from "@tanstack/react-query";
+import { Check, X } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { Fragment } from "react";
 
@@ -37,7 +38,12 @@ function ListingAmenities({ amenities }: Props) {
       return {
         id: `missing-${name}`,
         name,
-        display_name: masterAmenity?.display_name || name,
+        display_name:
+          masterAmenity?.display_name ||
+          name
+            .split("_")
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
       };
     });
 
@@ -51,23 +57,25 @@ function ListingAmenities({ amenities }: Props) {
       {matchedAmenities.map((amenity) => (
         <span
           key={amenity.id}
-          className="bg-green-100 text-green-700 text-xs font-medium px-2.5 py-0.5 rounded-full"
+          className="bg-green-100 text-green-700 text-xs font-medium pl-1.5 pr-2.5 py-0.5 rounded-full flex items-center"
         >
+          <Check size={16} strokeWidth={1} />
           {amenity.display_name}
         </span>
       ))}
       {missingAmenities.map((amenity) => (
         <span
           key={amenity.id}
-          className="bg-red-100 text-red-700 text-xs font-medium px-2.5 py-0.5 rounded-full"
+          className="bg-red-100 text-red-700 text-xs font-medium pl-1.5 pr-2.5 py-0.5 rounded-full flex items-center"
         >
+          <X size={16} strokeWidth={1} />
           {amenity.display_name}
         </span>
       ))}
       {extraAmenities.map((amenity) => (
         <span
           key={amenity.id}
-          className="bg-blue-100 text-blue-700 text-xs font-medium px-2.5 py-0.5 rounded-full"
+          className="bg-blue-100 text-blue-700 text-xs font-medium px-2.5 py-0.5 rounded-full flex items-center"
         >
           {amenity.display_name}
         </span>
