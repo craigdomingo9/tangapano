f543cbd621362e5984abe4b411ef04f781be3c61
craigdomingo9 <craigdomingo9@gmail.com>
2025-09-23 18:48:01 +0200

    django debug in production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 
 # Add the apps directory to the Python path
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
