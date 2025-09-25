78d4f2a3f429deb9937094a9606ec7b6d216d949
craigdomingo9 <craigdomingo9@gmail.com>
2025-09-25 16:53:37 +0200

    .

--- frontend/src/components/dashboard/listings/ListingsList.tsx
+++ frontend/src/components/dashboard/listings/ListingsList.tsx
@@ -11,7 +11,7 @@ function ListingsList({ listings }: Props) {
   return (
     <div
       className={cn(
-        "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 pb-4",
+        "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 place-items-center sm:place-items-start gap-6 pb-4",
         !isMobile &&
           state === "expanded" &&
           "sm:grid-cols-1 items-center place-items-center"
