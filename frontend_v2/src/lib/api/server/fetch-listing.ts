6b420638ccb6a21e7cf51407f979445d3fe59915
craigdomingo9 <craigdomingo9@gmail.com>
2025-12-02 08:06:03 +0200

    .

--- frontend_v2/src/lib/api/server/fetch-listing.ts
+++ frontend_v2/src/lib/api/server/fetch-listing.ts
@@ -8,7 +8,7 @@ export const getListingMetadata = cache(
       // and might need different base URL handling or direct DB access.
       const res = await fetch(
         `${
-          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost/api"
         }/listings/listing/${id}/`
       );
 
