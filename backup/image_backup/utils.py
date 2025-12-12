7a9c7e6a3f39cb8ba71237386b55433fb16f4622
craigdomingo9 <craigdomingo9@gmail.com>
2025-12-12 03:57:17 +0200

    .

--- backup/image_backup/utils.py
+++ backup/image_backup/utils.py
@@ -9,7 +9,7 @@ def get_logger():
             level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
             handlers=[
-                logging.FileHandler('backup_images.log'),
+                logging.FileHandler('/tmp/backup_images.log'),
                 logging.StreamHandler()
             ]
         )
--- restore/image_restore/utils.py
+++ restore/image_restore/utils.py
@@ -9,7 +9,7 @@ def get_logger():
             level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
             handlers=[
-                logging.FileHandler('restore_db.log'),
+                logging.FileHandler('/tmp/restore_images.log'),
                 logging.StreamHandler()
             ]
         )
