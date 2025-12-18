db96e1f6dfacddba2935fe6cdd8422ba7947ad42
Craig K Domingo <craigdomingo9@gmail.com>
2025-12-18 23:48:22 +0200

    Updates log file path to absolute location
    Ensures log files are written to a consistent directory
    by switching from a relative to an absolute path, which
    helps prevent file placement issues in different
    environments.

--- backup_v2/mixins/logger_mixins.py
+++ backup_v2/mixins/logger_mixins.py
@@ -3,13 +3,13 @@ import logging
 
 class LoggerMixin:
     def __init__(self):
-        os.makedirs('app/logs', exist_ok=True)
+        os.makedirs('/app/logs', exist_ok=True)
         environment = os.getenv("ENV", "production")
         log_level = logging.DEBUG if environment == "development" else logging.INFO
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        self.logger.handlers = [logging.StreamHandler(), logging.FileHandler('app/logs/backup.log')]
+        self.logger.handlers = [logging.StreamHandler(), logging.FileHandler('/app/logs/backup.log')]
         self.logger.handlers[0].setFormatter(formatter)
         self.logger.handlers[1].setFormatter(formatter)
 
