8f03495e482f4be383ac4d5cc8d28d34c8a18f4d
Craig K Domingo <craigdomingo9@gmail.com>
2025-12-18 03:12:32 +0200

    Updates environment variable names for database host and port
    Switches to using distinct environment variables for the connection
    pooler host and port to improve clarity and configurability of
    database connection settings.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -196,8 +196,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'root'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'pgbouncer'),
-        'PORT': os.environ.get('POSTGRES_PORT', 6432),
+        'HOST': os.environ.get('PG_BOUNCER_HOST', 'pgbouncer'),
+        'PORT': os.environ.get('PG_BOUNCER_PORT', 6432),
         'CONN_MAX_AGE': 0,
     }
 }
