a623f81ebfcb317bfede572ea6dc9a62c15db5c4
Craig K Domingo <craigdomingo9@gmail.com>
2025-12-21 04:46:41 +0200

    feat: Increase anonymous throttle rate from 50/hour to 100/hour.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,7 +147,7 @@ if not DEBUG:
         'rooms': '300/day',
         'inquiries': '100/hour',
         'user_lookup': '35/hour',
-        'anon': '50/hour',  # Global anonymous throttle
+        'anon': '100/hour',  # Global anonymous throttle
         'user': '1000/hour',  # Global authenticated throttle
     }
 else:
